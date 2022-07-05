e073cb760d08ec110af87aec0031b7e3ee7e2632
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2022-07-05 13:45:11 +0000

    Initial commit

---
+++ test/try-apigateway-with-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TryApigatewayWithCdk from '../lib/try-apigateway-with-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/try-apigateway-with-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TryApigatewayWithCdk.TryApigatewayWithCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
